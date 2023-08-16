87164ba806d36df41fe576f1572a3d0d676999b6
sadid56 <sadidhasan77@gmail.com>
2023-08-16 16:18:18 +0600

    added  a font family

--- main.js
+++ main.js
@@ -65,3 +65,42 @@ document.getElementById('number-input').addEventListener('input', function(){
     inputFieldElement.style.fontSize = numberInputField;
 })
 
+//! clear all
+document.getElementById('clear-btn').addEventListener('click', function(){
+    const inputFieldElement = document.getElementById('input-feild');
+    inputFieldElement.value = '';
+})
+ 
+
+
+//! font family
+document.getElementById('carosive-font').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('cursive');
+})
+document.getElementById('tilt-prism').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('tilt-prism-font');
+})
+document.getElementById('barriecito').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('barriecito-font');
+})
+document.getElementById('playfair').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('playfair-font');
+})
+document.getElementById('Pacifico').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('Pacifico-font');
+})
+document.getElementById('Rubik').addEventListener('click', function(){
+    
+    const inputFieldElement = document.getElementById('input-feild'); 
+    inputFieldElement.classList.toggle('Rubik-font');
+})
